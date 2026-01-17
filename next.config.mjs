04112fd1bef6aae76f4907ebee912ed442321efb
Muhammad Abubakar <sshaiy2255@gmail.com>
2026-01-17 22:29:27 +0500

    config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
-    domains: ["i.dummyjson.com", "dummyjson.com", "cdn.dummyjson.com","images.remotePatterns.com"],
+    domains: ["industrialideation.com"],
   },
 
 };
